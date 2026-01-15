b7c1a7818d95ada98c781ffd43c1c34ed2baddeb
Burrely <dinosammy1@live.nl>
2026-01-15 09:18:16 +0100

    Add 404 page and remove console.log
    Adds a default, root level 404 page and removes a console.log line in the Twitch Schedule loading script.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,3 @@
 const loadSchedule = async () => {
     return await fetch("https://twitchschedule.dinosammy1.workers.dev/").then(res => res.json()).then(res => res.data.segments)
 }
-
-loadSchedule().then(console.log)
